c5e5621d39be565e6aa2a503c8e7e20be6d46e6f
Boris Faure <boris@fau.re>
2024-09-02 23:34:46 +0200

    dummy main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use defmt::*;
+use defmt_rtt as _;
+use embassy_executor::Spawner;
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    info!("Hello, world!");
 }
